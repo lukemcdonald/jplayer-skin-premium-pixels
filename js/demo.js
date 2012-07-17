51f991de807482a9d3b9d7af3d4c63d000857ba1
thelukemcdonald <thelukemcdonald@gmail.com>
2012-07-17 11:58:09 -0500

    Add credits display

--- js/demo.js
+++ js/demo.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
 	
+	/**
+	 * General Variables
+	 *
+	 */
 	var skin_loader = $('#skin-loader');
 	var skin_wrapper = $('#skin-wrapper');
 	var skin_name = skin_wrapper.data('skin-name');
-	var skin_css_uri = 'skins/' + skin_name + '/' + skin_name + '.css';
+	var skin_credits = $('#credits');
+	var skin_uri = 'skins/' + skin_name;
+	
+	/**
+	 * Skin Styles and Display
+	 *
+	 */ 
+	var skin_css_uri = skin_uri + '/' + skin_name + '.css';
 	
 	/* Dynamically add skin stylesheet */
 	$('head').append('<link rel="stylesheet" href="' + skin_css_uri + '" type="text/css" />');
@@ -11,5 +22,22 @@ $(document).ready(function() {
 	/* Display Skin Wrapper */
 	skin_wrapper.fadeIn();
 	skin_loader.fadeOut();
+	
+	
+	/**
+	 * Credits Display
+	 *
+	 */ 
+	var skin_credits_url = skin_uri + '/credits.html';
+	
+	alert(newString);
+	
+	$.ajax({
+		url: skin_credits_url,
+		crossDomain: true,
+		cache: true
+	}).done(function( html ) {
+		skin_credits.append(html);
+	});
 	
 });
