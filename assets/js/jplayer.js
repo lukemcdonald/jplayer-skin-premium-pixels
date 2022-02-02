83a6ec782598333c18bfe97275add5d6ac5004ad
Luke McDonald <thelukemcdonald@gmail.com>
2022-02-02 11:49:29 -0600

    Updates

--- assets/js/jplayer.js
+++ assets/js/jplayer.js
@@ -9,25 +9,23 @@ jQuery.extend(demo, {
    * Initialize the jPlayer and playlist.
    */
 
-
   setupPlayer: () => {
     jPlaylist = new jPlayerPlaylist(
       {
         jPlayer: '#jquery_jplayer_1',
-        cssSelectorAncestor: '#jp_container_1'
+        cssSelectorAncestor: '#jp_container_1',
       },
       playlist,
       {
-				swfPath: 'assets/media',
-				supplied: 'oga, mp3',
-				wmode: 'window',
-				useStateClassSkin: true,
-				autoBlur: false,
-				keyEnabled: true
-			}
+        swfPath: 'assets/media',
+        supplied: 'oga, mp3',
+        wmode: 'window',
+        useStateClassSkin: true,
+        autoBlur: false,
+        keyEnabled: true,
+      }
     )
   },
-
 })
 
 demo.setupPlayer()
