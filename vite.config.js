2baab494ede6db7c208596d9d93557f80caa32ce
Luke McDonald <thelukemcdonald@gmail.com>
2022-02-02 11:49:14 -0600

    Fix Vite config base path.

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: 'src',
-  base: '/jplayer-skin-audiocheck/',
+  base: '/jplayer-premium-pixels/',
   build: {
     outDir: '../dist',
   },
